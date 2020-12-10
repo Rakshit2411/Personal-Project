a4c7bae805cdede0f9fb5eef8237a68123b8f45b
Rakshit Patel <Patel26@student.mitt.ca>
2020-12-09 23:03:04 -0600

    Minifying html, css, js and image files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
 const { src, dest, series, parallel } = require("gulp");
+const gulppostcss = require('gulp-postcss');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 // a function for HTML tasks
 function htmlTask() {
   return src('src/*.html')
-    .pipe('dist');
+    .pipe(dest('dist'))
 }
 
 // a function for JS file tasks
 function scriptTask() {
   return src('src/scripts/*.js')
-    .pipe('dist/scripts');
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .pipe(dest('dist/scripts'))
 }
 
 // a function for CSS files tasks
 function stylesTask() {
-  return src('src/*.css', 'src/styles/*.css')
-    .pipe('dist/styles');
+  return src('src/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(gulppostcss([ autoprefixer(), cssnano() ]))
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/styles'))
 }
 
 // a function for images tasks
 function imageTask() {
   return src('src/images/*.jpg', 'src/images/*.png', 'src/images/*.webp')
-    .pipe('dist/images');
+    .pipe(imagemin())
+    .pipe(dest('dist/images'))
 }
+
+// exports.html = htmlTask;
+// exports.styles = stylesTask;
+exports.default = series(htmlTask, imageTask, stylesTask, scriptTask);

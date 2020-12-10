0dd35b68b1789516145d992e42f2618430659a78
Rakshit Patel <Patel26@student.mitt.ca>
2020-12-09 23:27:53 -0600

    Update the javascript file

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,12 +5,19 @@ const email = document.querySelector("#email");
 const study = document.querySelector("#study");
 const address = document.querySelector("#address");
 const checkbox = document.querySelector("#agree");
+const array = [name, workField, email, study, address];
+console.log(name.classList);
 
-signupButton.addEventListener('click', function(){
-  name.value = '';
-  workField.value = '';
-  email.value = '';
-  study.value = '';
-  address.value = '';
-  checkbox.checked = false;
+signupButton.addEventListener('click', function () {
+  if (name.value != '' && workField.value != '' && email.value != '' && study.value != '' && address.value != '' && checkbox.checked != false) {
+    name.value = '';
+    workField.value = '';
+    email.value = '';
+    study.value = '';
+    address.value = '';
+    checkbox.checked = false;
+  } else {
+    alert("All fields are not complete.");
+  }
 })
+
